84496a4358c48380e7dd6261d6a719588792f5e7
Bitolinha <117381124+zatarana@users.noreply.github.com>
2026-05-17 17:41:37 -0300

    Add standard product categories

---
+++ src/lib/categories.ts
@@ -0,0 +1,12 @@
+export const STANDARD_CATEGORIES = [
+  'B01 - Iogurte',
+  'B02 - Margarina / Manteiga',
+  'B03 - Linguiças / Toscanas',
+  'B04 - Cortes Frango',
+  'B05 - Frutos do Mar / Hambúrguer / Pizza',
+  'B06 - Requeijão / Embutidos / Resfriados',
+  'B07 - Polpas',
+  'B08 - Sorvetes / Gelo',
+];
+
+export const DEFAULT_CATEGORY = STANDARD_CATEGORIES[0];
